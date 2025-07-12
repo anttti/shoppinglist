03698a34b160ca28fccf2a3f433928f84a2559af
Antti Mattila <antti.mattila@beehealthy.com>
2025-07-12 22:42:21 +0300

    add check_origin

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,8 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :shoppinglist, ShoppinglistWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["//kauppa.koeasema.com"]
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Shoppinglist.Finch
